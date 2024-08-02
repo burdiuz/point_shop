f2ccbf6e9591fd1e90a9fe1505100b821d46f835
burdiuz <burdiuz@gmail.com>
2024-08-02 20:36:51 +0300

    initial Flask setup

---
+++ app.py
@@ -0,0 +1,40 @@
+from flask import Flask
+from flask import render_template_string
+from flask import render_template
+
+app = Flask(__name__)
+
+name = "Vladyslav Olegovych"
+address = "Ukraine, Kyiv"
+people = ["oleg", "vladik", "davyd"]
+
+
+getPeople = lambda: people
+
+@app.route("/")
+def hello_world():
+    return "<p>Hello, {}!</p>".format(name)
+
+
+@app.route("/<verb>/<name>")
+def verb(verb, name):
+    if verb == "hello":
+        return "Hello {}.".format(name)
+    elif verb == "bye":
+        return "Bye {}.".format(name)
+    else:
+        return "Go away you fat little piece of shit."
+
+@app.route("/contacts")
+def api_contacts():
+    return render_template("contacts.html", address=address, pageTitle="Our Contacts", title="Contact address")
+
+@app.route("/noinfo")
+def api_noinfo():
+    return render_template("noinfo.html")
+
+@app.route("/people")
+def api_people():
+    return render_template("people.html", people=getPeople, pageTitle="Our People", title=render_template_string("People in our organsation ({{people()|length}})", people=getPeople))
+
+
