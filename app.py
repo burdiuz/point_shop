d11969366f945648dce6dd4378c0aaf866accc42
burdiuz <burdiuz@gmail.com>
2024-08-16 22:00:30 +0300

    Update people page to use api endpoint

--- app.py
+++ app.py
@@ -10,10 +10,12 @@ app = Flask(__name__)
 name = "Vladyslav Olegovych"
 address = "Ukraine, Kyiv"
 people = ["oleg", "vladik", "davyd"]
-age1 = 15
+age = 100500
 
 
 getPeople = lambda: people
+
+# ---- PAGES
 
 @app.route("/")
 def hello_world():
@@ -30,22 +32,27 @@ def verb(verb, name):
         return "Go away you fat little piece of shit."
 
 @app.route("/contacts")
-def api_contacts():
+def page_contacts():
     return render_template("contacts.html", address=address, pageTitle="Our Contacts", title="Contact address")
 
 @app.route("/info")
-def api_info():
+def page_info():
     return render_template("info.html", pageTitle="Random information", title="Some information to be listed")
 
 @app.route("/noinfo")
-def api_noinfo():
+def page_noinfo():
     return render_template("noinfo.html")
 
 @app.route("/people")
-def api_people():
-    return render_template("people.html", people=getPeople, pageTitle="Our People", title=render_template_string("People in our organsation ({{people()|length}})", people=getPeople))
+def page_people():
+    return render_template("people.html", pageTitle="Our People", title="People in our organsation")
 
 @app.route("/age")
-def api_age():
-    return render_template("age.html", age=age1, pageTitle="age", title="main developer's age is")
+def page_age():
+    return render_template("age.html", age=age, pageTitle="age", title="main developer's age is")
 
+# --- API ENDPOINTS
+
+@app.route("/api/people")
+def api_people():
+    return getPeople()
